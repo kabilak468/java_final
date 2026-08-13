b0ece4d78915203045c81b841219682ecce0428a
kabilak468 <kabik468@gmail.com>
2026-08-13 22:34:12 +0530

    created class and objects

--- codeio_oops/application_form.java
+++ codeio_oops/application_form.java
@@ -1,5 +1,33 @@
 //class & objects
 package codeio_oops;
+public class application_form{
+    String name;
+    int rollNo;
+    double mark;
+    public void display(){
+        System.out.println(name);
+        System.out.println(rollNo);
+        System.out.println(mark);
+    }
+    public void setCutoff(double cutoff){
+        mark=cutoff;
+    }
+    //Passing input through methods instead of variables
+    public static void main(String[] args){
+        //Object creation
+        application_form sc = new application_form();
+        sc.name="Kabi";
+        sc.rollNo=167;
+        sc.setCutoff(194.5);
+        sc.display();
+    }
+}
+
+
+//Creating class and objects
+
+/*
+package codeio_oops;
 
 public class application_form {
     String name;
@@ -17,3 +45,4 @@ public class application_form {
         app.display();
     }
 }
+*/
