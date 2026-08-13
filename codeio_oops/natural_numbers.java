2f937431eb85e0ae1a625f891744dc0821e58a35
kabilak468 <kabik468@gmail.com>
2026-08-13 21:51:04 +0530

    Recursive function

--- codeio_oops/natural_numbers.java
+++ codeio_oops/natural_numbers.java
@@ -1,15 +1,15 @@
-//Recursive function
+//Recursive Function
 package codeio_oops;
-public class natural_numbers {
+public class natural_numbers{
     public static void main(String args[]){
         natural(10);
     }
     public static void natural(int n){
-        //base class
+        //Base case
         if(n==1){
             System.out.println(1);
         }
-        //recursive class
+        //Recursive case
         else{
             System.out.println(n);
             natural(n-1);
