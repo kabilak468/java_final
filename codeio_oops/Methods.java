a67102a8a7c3d035ab242ecd0e2c39dcd53ffa8f
kabilak468 <kabik468@gmail.com>
2026-08-13 21:16:55 +0530

    Parameters inside methods

--- codeio_oops/Methods.java
+++ codeio_oops/Methods.java
@@ -1,11 +1,9 @@
 package codeio_oops;
 public class Methods{
     public static void main(String[] args){
-        add();
+        add(10,22);
     }
-    public static void add(){
-        int a=10;
-        int b=20;
+    public static void add(int a,int b){
         int c=a+b;
         System.out.println(c);
     }
