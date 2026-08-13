aec9a576771922264703afa6e92e1363cd5595fb
kabilak468 <kabik468@gmail.com>
2026-08-13 22:06:49 +0530

    first commit

--- codeio_oops/application_form.java
+++ codeio_oops/application_form.java
@@ -1,5 +1,19 @@
+//class & objects
 package codeio_oops;
 
 public class application_form {
-    
+    String name;
+    int rollNo;
+
+    public void display(){
+        System.out.println(name);
+        System.out.println(rollNo);
+    }
+
+    public static void main(String[] args){
+        application_form app =new application_form();
+        app.name="Kabi";
+        app.rollNo=167;
+        app.display();
+    }
 }
