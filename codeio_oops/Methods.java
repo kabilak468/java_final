971127f9d19b8341ca9a89a80ed5334c9fad9b61
kabilak468 <kabik468@gmail.com>
2026-08-13 21:26:43 +0530

    non void parameterised method

--- codeio_oops/Methods.java
+++ codeio_oops/Methods.java
@@ -1,10 +1,10 @@
 package codeio_oops;
 public class Methods{
     public static void main(String[] args){
-        add(10,22);
+        System.out.println(add(10,22));
     }
-    public static void add(int a,int b){
+    public static int add(int a,int b){
         int c=a+b;
-        System.out.println(c);
+        return c;
     }
 }
